046ae17398da562314dfe13da537efc493655ab7
Corno Schraverus <corno@schraverus.com>
2026-07-09 20:47:20 +0200

    pdt: ...

--- typescript/lib/src/implementation/transformers/csv/prose.ts
+++ typescript/lib/src/implementation/transformers/csv/prose.ts
@@ -4,7 +4,7 @@ import p_list_from_text from 'pareto-core/implementation/refiner/specials/list_f
 
 //data types
 import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/prose/data"
-import type * as d_in from "../../../../interface/data/csv.js"
+import type * as d_in from "../../../interface/data/csv.js"
 
 export namespace interface_ {
 
