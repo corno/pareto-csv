3d46d2e99aa1bb53aed525124fbb9903c87e3238
Corno Schraverus <corno@schraverus.com>
2026-08-18 15:18:39 +0200

    pdt: published version 0.1.17

---
+++ typescript/lib/src/schemas/nested/schema.ts
@@ -0,0 +1,11 @@
+import type * as p_ from 'pareto-core/interface/schema'
+
+import type * as s_out from "pareto-csv/schemas/csv/schema"
+
+export type Leaf_Dictionary = p_.Dictionary<s_out.Row>
+
+export type Composed_Dictionary = p_.Dictionary<Dictionary>
+
+export type Dictionary =
+    | ['leaf', Leaf_Dictionary]
+    | ['composed', Composed_Dictionary]
---
+++ typescript/lib/src/schemas/nested/transformers/csv.ts
@@ -0,0 +1,40 @@
+import * as p_ from 'pareto-core/implementation/transformer'
+
+import * as s_in from "../schema.js"
+import * as s_out from "../../csv/schema.js"
+
+    export const Leaf_Dictionary: p_.Transformer<
+        s_in.Leaf_Dictionary,
+        s_out.Rows
+    > = ($) => p_.from.dictionary($).convert_to_list(
+        ($, id): s_out.Row => ({
+            'cells': p_.literal.segmented_list([
+                p_.literal.list([id]),
+                $.cells,
+            ])
+        })
+    )
+
+    export const Composed_Dictionary: p_.Transformer<
+        s_in.Composed_Dictionary,
+        s_out.Rows
+    > = ($) => p_.from.dictionary($).flatten_to_list(
+        ($, id): s_out.Rows => p_.from.list(
+            p_.from.state($).decide(
+                ($): s_out.Rows => {
+                    switch ($[0]) {
+                        case 'composed': return p_.ss($, ($) => Composed_Dictionary($))
+                        case 'leaf': return p_.ss($, ($) => Leaf_Dictionary($))
+                        default: return p_.au($[0])
+                    }
+                }
+            )
+        ).map(
+            ($) => ({
+                'cells': p_.literal.segmented_list([
+                    p_.literal.list([id]),
+                    $.cells,
+                ])
+            })
+        )
+    )
